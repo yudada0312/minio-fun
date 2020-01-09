ac3c6c123fb823da226e19b0dee9b709a419bd75
yudada <yudada0312@gmail.com>
2020-01-09 14:05:31 +0800

    feat: 增加make bucket&list bucket

--- main.js
+++ main.js
@@ -39,21 +39,38 @@ server.get('/checkBucketExists', (req, res) => {
   console.log(req.query);
   client.bucketExists(req.query.bucket_name, function(err, exists) {
       if (err) {
-        console.log('bucketExists error');
-        console.log(err);
         res.send(err);
-      // return  reject('sserror');
       }else{
         if(exists){
-          console.log('bucketExists');
           res.send('bucketExists');
         }else{
-          console.log('bucketNoExists');
           res.send('bucketNoExists');
         }
       }
   });
 });
+
+server.get('/makeBucket', (req, res) => {
+  client.makeBucket(req.query.bucket_name, 'us-west-1', function(e) {
+    if (e) {
+      res.send(e);
+      return console.log(e)
+    }
+    console.log("Success")
+    res.send('Success');
+  })
+})
+
+server.get('/listBuckets', (req, res) => {
+  client.listBuckets(function(e, buckets) {
+    if (e){
+      res.send(e);
+      return console.log(e)
+    }
+    console.log('buckets :', buckets)
+    res.send(buckets);
+  })
+})
 
 server.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
